1e19f2614e8a1c9c33cdfcabe09e7a218f9dd7f2
AshanXT <109849631+AshanXT@users.noreply.github.com>
2022-07-24 17:07:41 +0530

    Login and registration Created

--- app/src/main/java/com/example/easytap/MainActivity.kt
+++ app/src/main/java/com/example/easytap/MainActivity.kt
@@ -4,11 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.easytap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.loginBtn.setOnClickListener {
+              startActivity(Intent(this,LoginActivity::class.java))
+        }
+
+        binding.skipBtn.setOnClickListener {
+            startActivity(Intent(this,DashboardUserActivity::class.java))
+        }
 
             }
 }
